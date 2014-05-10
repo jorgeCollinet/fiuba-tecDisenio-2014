f158cd0907b9bd818c44ae05953c39369de6828c
jorgeColl <jorgecollinet@hotmail.com>
2014-05-10 18:56:31 -0300

    agrego niveles (y pruebo que puedo commitear)

--- src/main/java/ar/fiuba/tecnicas/logging/Logger.java
+++ src/main/java/ar/fiuba/tecnicas/logging/Logger.java
@@ -1,5 +1,16 @@
 package ar.fiuba.tecnicas.logging;
 
+/**
+ * @author Grupo3
+ * 
+ */
 public class Logger {
-	
+
+	/**
+	 * 
+	 */
+	public Logger() {
+		// TODO Auto-generated constructor stub
+	}
+
 }
