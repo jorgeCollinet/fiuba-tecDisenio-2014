9bef6ab658f907e65c6b35edd56bdbb126d9391c
agustinorigone <agustin.origone@gmail.com>
2014-05-11 11:08:56 -0700

    prueba escribir archivo

--- src/main/java/ar/fiuba/tecnicas/logging/Logger.java
+++ src/main/java/ar/fiuba/tecnicas/logging/Logger.java
@@ -1,5 +1,23 @@
 package ar.fiuba.tecnicas.logging;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 public class Logger {
-	
+	public void writing() {
+	    try {
+	        File statText = new File("log.txt");
+	        FileOutputStream is = new FileOutputStream(statText);
+	        OutputStreamWriter osw = new OutputStreamWriter(is);    
+	        Writer w = new BufferedWriter(osw);
+	        w.write("POTATO!!!");
+	        w.close();
+	    } catch (IOException e) {
+	        System.err.println("Problem writing to the file statsTest.txt");
+	    }
+	}
 }
--- src/test/java/ar/fiuba/tecnicas/logging/LoggerTest.java
+++ src/test/java/ar/fiuba/tecnicas/logging/LoggerTest.java
@@ -1,10 +1,16 @@
 package ar.fiuba.tecnicas.logging;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 
 public class LoggerTest {
-
+	
+	private Logger logger = new Logger();
+	
+	@Test
+    public void log() {
+        logger.writing();
+        assertEquals(true, true);
+    }
 }
